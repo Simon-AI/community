fbcd8bf8fdcffdf0d95221aa15756e7d407e7ae4
yeximeng <425854220@qq.com>
2022-05-17 16:29:01 +0800

    like

--- src/main/java/com/question/community/controller/HomeController.java
+++ src/main/java/com/question/community/controller/HomeController.java
@@ -5,7 +5,9 @@ import com.question.community.entity.DiscussPost;
 import com.question.community.entity.Page;
 import com.question.community.entity.User;
 import com.question.community.service.DiscussPostService;
+import com.question.community.service.LikeService;
 import com.question.community.service.UserService;
+import com.question.community.util.CommunityConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,37 +20,44 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class HomeController {
+public class HomeController implements CommunityConstant {
     @Autowired
     private UserService userService;
 
     @Autowired
     private DiscussPostService discussPostService;
 
+    @Autowired
+    private LikeService likeService;
+
     @RequestMapping(path = "/index", method = RequestMethod.GET)
-    public String getIndexPage(Model model , Page page){
+    public String getIndexPage(Model model, Page page) {
         page.setRows(discussPostService.findDiscussPostRows(0));
         page.setPath("/index");
 
         List<DiscussPost> posts = discussPostService.findDiscussPosts(0, page.getOffset(), page.getLimit());
-        List<Map<String,Object>> discussPosts = new ArrayList<>();
+        List<Map<String, Object>> discussPosts = new ArrayList<>();
 
-        if(posts!=null){
-            for (DiscussPost post : posts){
-                Map<String,Object> map = new HashMap<>();
-                map.put("post",post);
+        if (posts != null) {
+            for (DiscussPost post : posts) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("post", post);
                 User user = userService.findUserById(post.getUserId());
-                map.put("user",user);
+                map.put("user", user);
+
+                long likeCount = likeService.findEntityLikeCount(ENTITY_TYPE_POST, post.getId());
+                map.put("likeCount", likeCount);
+
                 discussPosts.add(map);
             }
         }
 
-        model.addAttribute("discussPosts",discussPosts);
+        model.addAttribute("discussPosts", discussPosts);
         return "index";
     }
 
-    @RequestMapping(path = "/error",method = RequestMethod.GET)
-    public String getErrorPage(){
+    @RequestMapping(path = "/error", method = RequestMethod.GET)
+    public String getErrorPage() {
         return "/error/500";
     }
 
